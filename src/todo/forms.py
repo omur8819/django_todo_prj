7d0644e5b1c6eda0517b9af11cea50d324d4a6fc
omur8819 <omur8819@gmail.com>
2021-06-12 20:13:17 +0300

    feat: added CRUD operations

---
+++ src/todo/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.db.models import fields
+from .models import Todo
+
+class TodoAddForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ('title', )
+        
+class TodoUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ('title', 'completed')
+        # exclude = ('created_date')
